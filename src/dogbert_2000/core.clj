2d28421deb8c78944f49940372cf313780082301
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-02-01 13:36:42 -0200

    Configure hiccup

--- src/dogbert_2000/core.clj
+++ src/dogbert_2000/core.clj
@@ -1,12 +1,19 @@
 (ns dogbert-2000.core
   (:require [ring.adapter.jetty :as jetty]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response]]
+            [hiccup.page :refer [html5]]))
+
+(defn index-page []
+  (html5
+   [:head]
+   [:body
+    [:h1 "Dogbert 2000 (A Bit.ly clone)"]]))
 
 ; {:status 200
 ;  :headers {}
 ;  :body "Hello"} 
 (defn handle-hello [req]
-  (response "Hello"))
+  (response (index-page)))
 
 (defn -main [port]
   (jetty/run-jetty handle-hello {:port (Integer. port)}))
