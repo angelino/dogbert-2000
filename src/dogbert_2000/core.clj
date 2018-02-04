741923bed29463bd9206ceb4e1588c1ffe4c86d2
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2018-02-04 16:48:47 -0200

    Add the shorten form

--- src/dogbert_2000/core.clj
+++ src/dogbert_2000/core.clj
@@ -1,17 +1,25 @@
 (ns dogbert-2000.core
   (:require [ring.adapter.jetty :as jetty]
             [ring.util.response :refer [response]]
-            [hiccup.page :refer [html5]]))
+            [hiccup.page :refer [html5]]
+            [hiccup.core :refer [html]]
+            [hiccup.form :refer [form-to
+                                 text-field
+                                 submit-button]]))
+
+(defn shortener-form []
+  (html (form-to [:post "/urls"]
+                 (text-field "url")
+                 (submit-button "Shorten"))))
 
 (defn index-page []
   (html5
    [:head]
    [:body
-    [:h1 "Dogbert 2000 (A Bit.ly clone)"]]))
+    [:h1 "Dogbert 2000 (A Bit.ly clone)"]
+    [:div.content
+     (shortener-form)]]))
 
-; {:status 200
-;  :headers {}
-;  :body "Hello"} 
 (defn handle-hello [req]
   (response (index-page)))
 
