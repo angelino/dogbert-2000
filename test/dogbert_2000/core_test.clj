99a740d1325984e216dd83a6734beb5e550ea1e6
Lucas Angelino dos Santos <lucas.angelino@gmail.com>
2021-06-13 15:49:44 -0300

    Add some sample tests.

--- test/dogbert_2000/core_test.clj
+++ test/dogbert_2000/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [dogbert-2000.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest handle-index-url-test
+  (testing "GET '/' must show the index page with status 200."
+    (let [response (handle-index-url {})]
+      #_(prn response)
+      (is (= 200 (:status response)))))
+
+  (testing "GET '/urls' must show de index page with status 200."
+    (let [response (handle-index-url {})]
+      #_(prn response)
+      (is (= 200 (:status response))))))
